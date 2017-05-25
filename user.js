b0b768436818d10d43818696c8885f67d0ae00a8
nodiscc <nodiscc@gmail.com>
2017-05-25 03:15:04 +0200

    re-enable searching the default search engine from the url bar add notice about potential problems

--- user.js
+++ user.js
@@ -223,11 +223,12 @@ user_pref("clipboard.autocopy",					false);
 // PREF: Prevent leaking application locale/date format using JavaScript
 // https://bugzilla.mozilla.org/show_bug.cgi?id=867501
 // https://hg.mozilla.org/mozilla-central/rev/52d635f2b33d
-user_pref("javascript.use_us_english_locale",			true);
+user_pref("javascript.use_us_english_locale", 	true);
 
-// PREF: Do not submit invalid URIs entered in the address bar to the default search engine
+// PREF: Do not submit invalid URIs entered in the address bar to the default search engine (disabled)
+// NOTICE: Mistyping an URL could result in leaking it to your default search engine
 // http://kb.mozillazine.org/Keyword.enabled
-user_pref("keyword.enabled",					false);
+//user_pref("keyword.enabled",					false);
 
 // PREF: Don't trim HTTP off of URLs in the address bar.
 // https://bugzilla.mozilla.org/show_bug.cgi?id=665580
