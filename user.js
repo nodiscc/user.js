3a85571a7948df080624688c6da797f5d01cfae3
nodiscc <nodiscc@gmail.com>
2018-02-21 19:24:57 +0100

    re-enable all webGL functionality

--- user.js
+++ user.js
@@ -167,18 +167,22 @@ user_pref("dom.enable_resource_timing",				false);
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1342361
 user_pref("dom.archivereader.enabled",				false);
 
-// PREF: Disable webGL
+// PREF: Disable webGL (disabled)
 // https://en.wikipedia.org/wiki/WebGL
 // https://www.contextis.com/resources/blog/webgl-new-dimension-browser-exploitation/
-user_pref("webgl.disabled",					true);
-// PREF: When webGL is enabled, use the minimum capability mode
-user_pref("webgl.min_capability_mode",				true);
-// PREF: When webGL is enabled, disable webGL extensions
+// user_pref("webgl.disabled",					true);
+
+// PREF: When webGL is enabled, use the minimum capability mode (disabled)
+// user_pref("webgl.min_capability_mode",				true);
+
+// PREF: When webGL is enabled, disable webGL extensions (disabled)
 // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API#WebGL_debugging_and_testing
-user_pref("webgl.disable-extensions",				true);
-// PREF: When webGL is enabled, force enabling it even when layer acceleration is not supported
+//user_pref("webgl.disable-extensions",				true);
+
+// PREF: When webGL is enabled, force enabling it even when layer acceleration is not supported (disabled)
 // https://trac.torproject.org/projects/tor/ticket/18603
-user_pref("webgl.disable-fail-if-major-performance-caveat",	true);
+// user_pref("webgl.disable-fail-if-major-performance-caveat",	true);
+
 // PREF: When webGL is enabled, do not expose information about the graphics driver
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1171228
 // https://developer.mozilla.org/en-US/docs/Web/API/WEBGL_debug_renderer_info
